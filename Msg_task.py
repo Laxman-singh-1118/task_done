8bcc46d769376899aa5482a4751a4017fbe2bdb3
Laxman-singh-1118 <laksa1118@gmail.com>
2025-07-08 13:13:10 +0530

    Update Msg_task.py

--- Msg_task.py
+++ Msg_task.py
@@ -1,1 +1,17 @@
 
+from twilio.rest import Client
+
+# Credentials
+account_sid = input("put ur sid of twilio")
+auth_token = input('pls put ur token')# KEEP THIS SAFE!
+
+client = Client(account_sid, auth_token)
+
+# Send SMS
+message = client.messages.create(
+    body="Hello from Python using Twilio!",
+    from_=input("Your Twilio phone number")
+    to=input('Destination phone number')
+)
+
+print("Message SID:", message.sid)
